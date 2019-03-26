a4ba34aea5d97e2e9c3b5ad94f42b06a9f0e36ff
Bruno Bernardo <bruno@xhail.com>
2019-03-26 12:04:09 +0000

    stage 2 until 20 brhc

---
+++ stats.js
@@ -0,0 +1,8 @@
+const ops = require("./docs/ops.json");
+const opTypes = require("./docs/opTypes.json");
+
+console.log(`Number of ops: ${ops.length}`);
+opTypes.forEach(opType => {
+    const opsOfType = ops.filter(op => op.type === opType.id);
+    console.log(`${opsOfType.length}: ${opType.name} (${opType.id})`);
+});
