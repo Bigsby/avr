f590116f4992dfcaab0e6c596d3d1c421803ae07
Bruno Bernardo <bruno@xhail.com>
2019-03-28 10:36:28 +0000

    more analysis

--- ops.js
+++ ops.js
@@ -11,7 +11,88 @@ function firstWord(op) {
 }
 
 ops.sort((op1, op2) => firstWord(op1) > firstWord(op2) ? 1 : -1);
+// ops.forEach(op => {
+//     if (firstWord(op))
+//     console.log(`${firstWord(op)} : ${op.type} : ${op.syntax} : ${op.name}`);
+// });
+
+const operands = [];
+
+function addOperand(operandName, restriction) {
+    let operand = operands.find(op => op.name === operandName);
+    if (!operand) {
+        operand = {
+            name: operandName,
+            restrictions: []
+        }
+        operands.push(operand);
+    }
+    if (!operand.restrictions.includes(restriction)) {
+        operand.restrictions.push(restriction);
+    }
+    return operand;
+}
+
+function areArraysEqual(array1, array2) {
+    if (array1.length != array2.length) {
+        return false;
+    }
+
+    for (let index = 0; index < array1.length; index++) {
+        if (array1[index] !== array2[index]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function addIndexesToOperand(operand, indexes) {
+    if (!operand.indexes) {
+        operand.indexes = [];
+    }
+
+    let exists = false;
+    operand.indexes.forEach(existing => exists |= areArraysEqual(indexes, existing));
+    if (!exists) {
+        operand.indexes.push(indexes);
+    }
+}
+
+function getPositionsInOpCode(operand, opCode) {
+    if (opCode) {
+        const indexes = [];
+        let spacesFound = 0;
+        for (let codeIndex = 0; codeIndex < opCode.length; codeIndex++) {
+            if (opCode[codeIndex] === operand.name) {
+                indexes.push(codeIndex - spacesFound);
+            }
+            if (opCode[codeIndex] === " ") {
+                spacesFound++;
+            }
+        }
+        addIndexesToOperand(operand, indexes);
+    }
+}
+
+function getPositionsInOperation(operand, op) {
+    if (Array.isArray(op.opCode)) {
+        op.opCode.forEach(opCode => getPositionsInOpCode(operand, opCode));
+    } else {
+        getPositionsInOpCode(operand, op.opCode);
+    }
+}
+
 ops.forEach(op => {
-    if (firstWord(op))
-    console.log(`${firstWord(op)} : ${op.type} : ${op.syntax} : ${op.name}`);
+    if (op.operands) {
+        for (const operandName in op.operands) {
+            const operand = addOperand(operandName, op.operands[operandName]);
+            getPositionsInOperation(operand, op);
+        }
+    }
+});
+
+operands.forEach(operand => {
+    console.log(`${operand.name}: ${operand.restrictions}`);
+    operand.indexes.forEach(index => console.log(`  ${index}`));
+    console.log();
 });
