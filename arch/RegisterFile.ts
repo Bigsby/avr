aa07f87ff0a82f69e62ef3a700a357686076116c
Bruno Bernardo <bruno@xhail.com>
2019-04-10 12:59:10 +0100

    add clock and flash

--- arch/RegisterFile.ts
+++ arch/RegisterFile.ts
@@ -3,9 +3,9 @@ import Register from "./register";
 export default class RegisterFile {
     registers: Register[];
 
-    constructor(private registerCount: number, public registerWidth: number) {
+    constructor(private REGISTER_COUNT: number, public registerWidth: number) {
         this.registers = [];
-        for (let registerIndex = 0; registerIndex < registerCount; registerIndex++) {
+        for (let registerIndex = 0; registerIndex < REGISTER_COUNT; registerIndex++) {
             this.registers.push(new Register());
         }
     }
--- arch/base/mcu.ts
+++ arch/base/mcu.ts
@@ -2,7 +2,7 @@ export default abstract class MCU {
     constructor(
         public flash_size: number,
         public eeprom_size: number,
-        public ram_size: number,
+        public RAM_SIZE: number,
         public data_bus_width: number,
         public interrupt_vector_size: number,
         public boot_size: number
