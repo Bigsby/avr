492333a72780353181a36596b76b57a0c52e889d
Bruno Bernardo <bigsby@live.com>
2019-05-16 01:01:08 +0100

    assembling and disassembling

---
+++ test/compile/main.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+    while (1);
+}
+
+
+// https://blog.podkalicki.com/how-to-compile-and-burn-the-code-to-avr-chip-on-linuxmacosxwindows/
+// avr-gcc -Wall -g -Os -mmcu=attiny85 -o main.bin .\main.c
+// avr-size -C main.bin
+// avr-objcopy -j .text -j .data -O ihex main.bin main.hex
+// avr-objdump -s -m <avr architecture> -d program.hex > program.dump
+// avr-objdump -s -m avr -d main.hex > main.dump
