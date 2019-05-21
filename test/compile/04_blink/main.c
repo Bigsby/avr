d6c7f20717b36b92274b2119880aae555d5b9a6c
Bruno Bernardo <bruno@xhail.com>
2019-05-21 14:32:03 +0100

    add notes

--- test/compile/04_blink/main.c
+++ test/compile/04_blink/main.c
@@ -18,6 +18,7 @@ int main(void)
 
 // https://blog.podkalicki.com/how-to-compile-and-burn-the-code-to-avr-chip-on-linuxmacosxwindows/
 // avr-gcc -Wall -g -Os -mmcu=attiny85 -o main.bin .\main.c
+// avr-gcc -Wall -g -Os -mmcu=atmega328 -o main.bin ./main.c
 // avr-size -C main.bin
 // avr-objcopy -j .text -j .data -O ihex main.bin main.hex
 // avr-objdump -s -m <avr architecture> -d program.hex > program.dump
